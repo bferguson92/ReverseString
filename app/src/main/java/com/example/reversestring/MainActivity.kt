331d72feb892c7d8ae02c1de4059838b22528721
brianferguson <brian.ferguson@mobileappscompany.us>
2019-09-26 19:43:25 -0400

    Initial commit

---
+++ app/src/main/java/com/example/reversestring/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.reversestring
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(view: View){
+        when(view.id){
+            R.id.btnReverse -> showReversedString();
+        }
+    }
+
+    fun showReversedString(){
+        var str = etString.text.toString();
+        str = str.reversed()
+
+        tvReversedString.text = str;
+    }
+
+}
